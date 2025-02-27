966752f6514d7adeccf8f2d8696eff58db41f6a1
jonasgabrieel <jonasgabrieel@gmail.com>
2025-02-27 01:39:18 -0300

    Validação de forms e mensagem de sucesso

--- menu.js
+++ menu.js
@@ -23,3 +23,45 @@ document.addEventListener("DOMContentLoaded", function() {
         }, index * 150); // Delay para cada card
     });
 });
+
+document.getElementById('telefone').addEventListener('input', function (e) {
+    let value = e.target.value.replace(/\D/g, '');
+    if (value.length > 10) {
+        e.target.value = value.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    } else {
+        e.target.value = value.replace(/(\d{2})(\d{4})(\d{0,4})/, '($1) $2-$3');
+    }
+});
+
+document.getElementById('contato').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let form = e.target;
+    let formData = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: { 'Accept': 'application/json' }
+    }).then(response => {
+        if (response.ok) {
+            let toast = document.getElementById('toast');
+            toast.style.display = 'block';
+
+            // Remove após 4 segundos
+            setTimeout(() => {
+                toast.style.animation = 'fadeOut 0.5s ease-in-out';
+                setTimeout(() => {
+                    toast.style.display = 'none';
+                    toast.style.animation = 'fadeIn 0.5s ease-in-out'; // Reseta a animação
+                }, 500);
+            }, 4000);
+
+            form.reset();
+        } else {
+            alert("Erro ao enviar o formulário. Tente novamente.");
+        }
+    }).catch(error => {
+        alert("Erro ao enviar o formulário. Verifique sua conexão.");
+    });
+});
